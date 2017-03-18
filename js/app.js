d7bfb1e689a2867b4fec6a268af6fd2129c909b4
Jessica Gilligan <jessicagilligan@lawn-143-215-61-40.lawn.gatech.edu>
2017-03-18 14:32:18 -0400

    added additional ajax needs and urls

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 //var input = "";
  //get map
  var placesAddress ='';
+ var cityStateZip = "";
+ var streetAddress = "";
  function initAutocomplete() {
          var map = new google.maps.Map(document.getElementById('map'), {
            center: {lat: 32.0812053, lng: -81.0934179},
@@ -73,7 +75,7 @@
 
          });
        }
-       $(document).on('click', '#search', function(){
+       $(document).on('click', '#search', function getGoogleData(){
        console.log("Address 1" + placesAddress);
        var placesAddressFormatted = placesAddress.replace(/ /g, "+");
        console.log ("Address formatted " + placesAddressFormatted);
@@ -86,7 +88,7 @@
 
 
 
-      $.ajax({
+      var myAjaxCall = $.ajax({
             url: queryURLGoogle,
               method: "GET"
          })
@@ -124,27 +126,54 @@
            var streetAddress = response1.results[0].address_components[0].long_name + "+" +
                                response1.results[0].address_components[1].long_name.replace(/ /g, "+");
            console.log(streetAddress);
+
+           var city = response1.results[0].address_components[cityIndex].long_name;
+           var state = response1.results[0].address_components[stateIndex].long_name;
+
            var cityStateZip = response1.results[0].address_components[cityIndex].long_name.replace(/ /g, "+")  + "+" +
                              response1.results[0].address_components[stateIndex].long_name.replace(/ /g, "+")  + "+" +
                              response1.results[0].address_components[zipIndex].long_name;
-           console.log(cityStateZip);
+           console.log("Here it is!!!! "+cityStateZip);
+           getZillowData(city, state);
+           //getZillowData(streetAddress, cityStateZip);
          });
+  });
+// }
+// function doSomethingLater(fn, time ) {
+//   var dfd = $.Deferred();
+//
+//   setTimeout(function() {
+//     dfd.resolve( getGoogleData());
+//   }, time || 0);
+//
+//   return dfd.promise();
+// }
+//   var promise = doSomethingLater(getZillowData(), 5000);
+  // end of on-click
 
+
+    function getZillowData(city, state) {
+    console.log(streetAddress)
      // Event listener for all button element
      $.ajaxPrefilter(function(options) {
          if (options.crossDomain && jQuery.support.cors) {
              options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
          }
      });
-
+console.log('http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz19b7ds3exor_305s0&state='+state+'&city='+city+'&childtype=neighborhood');
+//console.log('http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19b7ds3exor_305s0&address='+streetAddress+'&citystatezip='+cityStateZip);
      $.ajax({
              dataType: "xml",
-             zillowURL: 'https://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19b7ds3exor_305s0&address='+streetAddress+'&citystatezip='+cityStateZip,
+             //url: 'http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19b7ds3exor_305s0&address='+streetAddress+'&citystatezip='+cityStateZip,
+             url: 'http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz19b7ds3exor_305s0&state='+state+'&city='+city+'&childtype=neighborhood',
              method: "GET"
          })
          // After the data comes back from the API
          .done(function(response2) {
            console.log(response2);
+           var jsonData = $.xml2json(response2);
+                var parsedResult = jsonData["#document"]["RegionChildren:regionchildren"].response.list;
+                //var parsedResult = jsonData["#document"]["SearchResults:searchresults"].response.results.result;
+                console.log(parsedResult); //Look in console.
          });
-// end of on click
-      });
+       };
