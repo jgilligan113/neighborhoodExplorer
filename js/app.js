34f145c0241b183ca8348d35e4ecd01a6e2d3c5c
singh <webexpertsingh@gmail.com>
2017-03-21 19:43:13 -0400

    removed console logs

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ var neighborhood = '';
 var database = firebase.database();
 database.ref().on("child_added", function(childSnapshot){
  var obj = childSnapshot.val();
-  console.log(obj);
+  //console.log(obj);
   $('#myTable thead:last').after('<tr><td>'+ obj.streetAddress2 +'</td><td>'+ obj.city2 +'</td><td>'+ obj.state2 +'</td><td>'+ obj.neighborhood2+'</td>');
 }, function(errorObject) {
   console.log("The read failed: " + errorObject.code);
@@ -52,7 +52,7 @@ database.ref().on("child_added", function(childSnapshot){
          searchBox.addListener('places_changed', function() {
            var places = searchBox.getPlaces();
            placesAddress = places[0].formatted_address;
-           console.log(placesAddress);
+           //console.log(placesAddress);
            test111(placesAddress);
 
 
@@ -103,17 +103,17 @@ database.ref().on("child_added", function(childSnapshot){
        }
        //$(document).on('click', '#search', function getGoogleData(){
        function test111(placesAddress){
-        console.log("inside test finction");
+        //console.log("inside test finction");
        //event.preventDefault();
-       console.log("Address 1" + placesAddress);
+       //console.log("Address 1" + placesAddress);
        var placesAddressFormatted = placesAddress.replace(/ /g, "+");
-       console.log ("Address formatted " + placesAddressFormatted);
+       //console.log ("Address formatted " + placesAddressFormatted);
        $(".progress").css('display', 'block');
 
      //api call to google
 
       var queryURLGoogle = 'https://maps.googleapis.com/maps/api/geocode/json?address='+ placesAddressFormatted +'&key=AIzaSyCm1RMUd3GGzW25X_Vl463MG-qlQ_CEkQg';
-      console.log(queryURLGoogle);
+      //console.log(queryURLGoogle);
 
 
 
@@ -122,39 +122,39 @@ database.ref().on("child_added", function(childSnapshot){
               method: "GET"
          })
          .done(function(response1) {
-           console.log(response1);
+           //console.log(response1);
 //look thru object to find city, state and zip array index value
 
            for (i = 0; i < response1.results[0].address_components.length; i++ ) {
 
               if (response1.results[0].address_components[i].types[0] == "locality") {
                 var cityIndex = i;
-                console.log(cityIndex);
+                //console.log(cityIndex);
               }
                 else if
                   (response1.results[0].address_components[i].types[0] == "administrative_area_level_1") {
                     var stateIndex = i;
-                    console.log(stateIndex);
+                    //console.log(stateIndex);
                   }
                   else if
                     (response1.results[0].address_components[i].types[0] == "postal_code") {
                       var zipIndex = i;
-                      console.log(zipIndex);
+                      //console.log(zipIndex);
                     }
 
                else { console.log("I can't find it")};
 
            };
 
-           console.log(response1.results[0].address_components[0].long_name);
-           console.log(response1.results[0].address_components[1].long_name);
-           console.log(response1.results[0].address_components[cityIndex].long_name);
-           console.log(response1.results[0].address_components[stateIndex].long_name);
-           console.log(response1.results[0].address_components[zipIndex].long_name);
+           //console.log(response1.results[0].address_components[0].long_name);
+           //console.log(response1.results[0].address_components[1].long_name);
+           //console.log(response1.results[0].address_components[cityIndex].long_name);
+           //console.log(response1.results[0].address_components[stateIndex].long_name);
+           //console.log(response1.results[0].address_components[zipIndex].long_name);
 
            var streetAddress = response1.results[0].address_components[0].long_name + "+" +
                                response1.results[0].address_components[1].long_name.replace(/ /g, "+");
-           console.log(streetAddress);
+           //console.log(streetAddress);
 
            var city = response1.results[0].address_components[cityIndex].long_name;
            var state = response1.results[0].address_components[stateIndex].long_name;
@@ -162,7 +162,7 @@ database.ref().on("child_added", function(childSnapshot){
            var cityStateZip = response1.results[0].address_components[cityIndex].long_name.replace(/ /g, "+")  + "+" +
                              response1.results[0].address_components[stateIndex].long_name.replace(/ /g, "+")  + "+" +
                              response1.results[0].address_components[zipIndex].long_name;
-           console.log("Here it is!!!! "+cityStateZip);
+           //console.log("Here it is!!!! "+cityStateZip);
 
            getZillowData(city, state, streetAddress, cityStateZip);
          });
@@ -172,8 +172,8 @@ database.ref().on("child_added", function(childSnapshot){
 
     function getZillowData(city, state, streetAddress, cityStateZip) {
     $('.sourceInfo').html("");
-    console.log(streetAddress);
-    console.log()
+    //console.log(streetAddress);
+    //console.log()
 
      // Event listener for all button element
      $.ajaxPrefilter(function(options) {
@@ -181,9 +181,9 @@ database.ref().on("child_added", function(childSnapshot){
              options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
          }
      });
-console.log('http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz19b7ds3exor_305s0&state='+state+'&city='+city+'&childtype=neighborhood');
-console.log('http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19b7ds3exor_305s0&address='+streetAddress+'&citystatezip='+cityStateZip);
-console.log('http://api.wunderground.com/api/2d160d5a7d89cd60/geolookup/conditions/q/'+state+'/'+city+'.json');
+//console.log('http://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz19b7ds3exor_305s0&state='+state+'&city='+city+'&childtype=neighborhood');
+//console.log('http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19b7ds3exor_305s0&address='+streetAddress+'&citystatezip='+cityStateZip);
+//console.log('http://api.wunderground.com/api/2d160d5a7d89cd60/geolookup/conditions/q/'+state+'/'+city+'.json');
 
 //get list of neighborhoods for address entered using api call to zillow using Region Children url
      $.ajax({
@@ -193,12 +193,12 @@ console.log('http://api.wunderground.com/api/2d160d5a7d89cd60/geolookup/conditio
          })
          // After the data comes back from the API
          .done(function(response2) {
-           console.log(response2);
+           //console.log(response2);
            var jsonData = $.xml2json(response2);
                 var parsedResult = jsonData["#document"]["RegionChildren:regionchildren"].response.list;
-                console.log(parsedResult); //Look in console.
-                console.log(streetAddress, cityStateZip);
-                console.log(parsedResult.region[0].name);
+                //console.log(parsedResult); //Look in console.
+                //console.log(streetAddress, cityStateZip);
+                //console.log(parsedResult.region[0].name);
 
                 $('#neighborhoods').on('click', function(){
                      event.preventDefault();
@@ -209,7 +209,7 @@ console.log('http://api.wunderground.com/api/2d160d5a7d89cd60/geolookup/conditio
                      var neighborhoodList = '<li>'+parsedResult.region[i].name+'</li>';
                      listDiv.append(neighborhoodList);
                      $('.sourceInfo').html(listDiv)
-                     console.log(neighborhoodList);
+                     //console.log(neighborhoodList);
                    }
                 });
 
@@ -220,11 +220,11 @@ console.log('http://api.wunderground.com/api/2d160d5a7d89cd60/geolookup/conditio
                     url : 'http://api.wunderground.com/api/2d160d5a7d89cd60/geolookup/forecast/q/'+state+'/'+city+'.json',
                     method: 'GET'
                     }).done(function (weather){
-                      console.log('F High------------'+weather.forecast.simpleforecast.forecastday[0].high.fahrenheit);
-                      console.log('C High------------'+weather.forecast.simpleforecast.forecastday[0].high.celsius);
-                      console.log('F Low-------------'+weather.forecast.simpleforecast.forecastday[0].low.fahrenheit);
-                      console.log('C Low-------------'+weather.forecast.simpleforecast.forecastday[0].low.celsius);
-                      console.log('conditions-------------'+weather.forecast.txt_forecast.forecastday[0].fcttext);
+                      //console.log('F High------------'+weather.forecast.simpleforecast.forecastday[0].high.fahrenheit);
+                      //console.log('C High------------'+weather.forecast.simpleforecast.forecastday[0].high.celsius);
+                      //console.log('F Low-------------'+weather.forecast.simpleforecast.forecastday[0].low.fahrenheit);
+                      //console.log('C Low-------------'+weather.forecast.simpleforecast.forecastday[0].low.celsius);
+                      //console.log('conditions-------------'+weather.forecast.txt_forecast.forecastday[0].fcttext);
                       $('#city').text(city);
                       $('#high').text(weather.forecast.simpleforecast.forecastday[0].high.fahrenheit);
                       $('#low').text(weather.forecast.simpleforecast.forecastday[0].low.fahrenheit);
@@ -241,7 +241,7 @@ console.log('http://api.wunderground.com/api/2d160d5a7d89cd60/geolookup/conditio
                     })
                     // After the data comes back from the API - this is hte zillow deep search results where we get details on specific address.
                     .done(function(response3) {
-                      console.log(response3);
+                      //console.log(response3);
                       $(".progress").css('display', 'none');
                       var jsonData2 = $.xml2json(response3);
                            var parsedResultError = jsonData2["#document"]["SearchResults:searchresults"].message.text;
@@ -251,15 +251,15 @@ console.log('http://api.wunderground.com/api/2d160d5a7d89cd60/geolookup/conditio
                               $('#modal1').modal('open');
                               //$('.modal-content').find('p').text(message);
 
-                           console.log(parsedResultError);
+                           //console.log(parsedResultError);
                          } else {
                            var parsedResult2 = jsonData2["#document"]["SearchResults:searchresults"].response.results.result;
 
-                             console.log(parsedResult2);
+                             //console.log(parsedResult2);
                              var streetAddress2 = parsedResult2.address.street;
                              var city2 = parsedResult2.address.city;
                              var state2 = parsedResult2.address.state;
-                             console.log(streetAddress);
+                             //console.log(streetAddress);
                              var bedrooms = parsedResult2.bedrooms;
                              var bathrooms = parsedResult2.bathrooms;
                              var lastSold = parsedResult2.lastSoldPrice._;
@@ -269,7 +269,7 @@ console.log('http://api.wunderground.com/api/2d160d5a7d89cd60/geolookup/conditio
                              var data = parsedResult2.links.graphsanddata;
                              var details = parsedResult2.links.homedetails;
                              var map = parsedResult2.links.mapthishome;
-                             console.log(neighborhood2);
+                             //console.log(neighborhood2);
                              var addressDetails = $("<div>").attr("class", "addressDetails");
                              addressDetails.html('<h5>Home Details</h5><p>Bedrooms: '+bedrooms+'<br>Bathrooms: '+bathrooms+'<br>Finished Square Feet: '+sqrFt+'<br>Last Sold for: $'+lastSold+'<br>Neighborhood: '+neighborhood2+'</p>');
                              $('.sourceInfo').html("");
